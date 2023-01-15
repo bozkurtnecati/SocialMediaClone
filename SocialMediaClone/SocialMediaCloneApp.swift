4912e541f29b44386825556e16744d782edd7688
bozkurtnecati <bozkurtnecati07@hotmail.com>
2023-01-15 19:38:47 +0300

    Initial Commit

---
+++ SocialMediaClone/SocialMediaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaCloneApp.swift
+//  SocialMediaClone
+//
+//  Created by Necati Bozkurt on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
